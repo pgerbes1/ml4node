664b1c2fcbaec4703e205a84cc2dbbe6bb833166
Patrick Gerbes <patrickgerbes@Patricks-MacBook-Pro-2.local>
2016-09-29 17:51:00 -0400

    Simplify logic for checking is array is all numbers

--- test/utils.js
+++ test/utils.js
@@ -6,11 +6,11 @@ var utils = require('../lib/utils');
 var numericArray = [1, 2, 3]; 
 var nonNumericArray = [1, 2, 'Not Number'];
 
-describe('The has arrayHasNonNumericValues function', function() {
-	it('should return false for arrays of all numbers', function() {
-		expect(utils.arrayHasNonNumerics(numericArray)).to.equal(false);
+describe('The has isNumericArray function', function() {
+	it('should return true for arrays of all numbers', function() {
+		expect(utils.isNumericArray(numericArray)).to.equal(true);
 	});
-	it('should return true for arrays with non-numbers', function() {
-		expect(utils.arrayHasNonNumerics(nonNumericArray)).to.equal(false);
+	it('should return false for arrays with non-numbers', function() {
+		expect(utils.isNumericArray(nonNumericArray)).to.equal(false);
 	});
 });
