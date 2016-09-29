b345c11f320a2f8a7d0834373385d0c0b583c9b7
Patrick Gerbes <patrickgerbes@Patricks-MacBook-Pro-2.local>
2016-09-29 17:55:34 -0400

    Add tests for Matrix type

---
+++ test/matrix.js
@@ -0,0 +1,33 @@
+'use strict'; 
+
+var expect = require('chai').expect;
+var Matrix = require('../lib/matrix');
+
+var m = new Matrix([1, 2, 3, 4], 2, 2); 
+
+console.log(m);
+console.log(m.dimensions());
+
+describe('Instantiating a matrix', function() {
+	it('should throw an error if not enough data to construct', function() {
+		expect(function() { 
+			  Matrix([1, 1, 1, 1], 2, 3);
+		}).to.throw(Error);
+	}); 
+	it('should throw an error for non-numerical arguments', function() {
+		expect(function() { 
+			  Matrix([1, 1, 1,'cat'], 2, 2);
+		}).to.throw(Error);
+	});
+	it('should throw an error for bad col or row arguments', function() {
+		expect(function() { 
+			  Matrix([1, 1, 1, 1], 2);
+		}).to.throw(Error);
+	});
+}); 
+
+describe('The dimensions of a matrix', function() {
+	it('should be an array with its cols and rows', function() {
+		expect(m.dimensions()).to.deep.equal([2, 2]);
+	});
+});
